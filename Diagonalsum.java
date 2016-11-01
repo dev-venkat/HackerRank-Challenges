f088c2eab8e06e8e918b932f5844e495ab89cdcf
dev-venkat <devven.github@gmail.com>
2016-10-31 22:14:14 -0400

    Add files via upload

---
+++ Diagonalsum.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Diagonalsum {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[][] = new int[n][n];
+        int sum1,sum2;
+        sum1=0;
+        sum2=0;
+        
+        for(int a_i=0; a_i < n; a_i++){
+            for(int a_j=0; a_j < n; a_j++){
+                a[a_i][a_j] = in.nextInt();
+            }
+        }
+         for(int a_i=0; a_i < n; a_i++){
+                sum1 += a[a_i][a_i];
+        }
+        for(int a_i=0,a_j=n-1; a_i < n && a_j > -1; a_i++,a_j--){
+                sum2 += a[a_i][a_j];
+        }
+		
+        System.out.println(Math.abs(sum1-sum2));
+    }
+}
